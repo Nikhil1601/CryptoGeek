5ce38aac73e5c25284a4d1dd4709adc9771430b3
Nikhil gautam <90842808+Nikhil1601@users.noreply.github.com>
2022-12-30 22:45:53 +0530

    Updated files

--- src/components/Coinapp.js
+++ src/components/Coinapp.js
@@ -9,7 +9,7 @@ const Coinapp = () => {
     useEffect(() => {
       axios
         .get(
-          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false'
         )
         .then(res => {
           setCoins(res.data);
