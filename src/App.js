b298265605a0c7716cc3dfb33fd69da82393e809
Nikhil gautam <90842808+Nikhil1601@users.noreply.github.com>
2022-12-30 20:30:57 +0530

    Updated browser routes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home />} exact />
           <Route path="/News" element={<News />} exact />
